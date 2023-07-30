8389b075d892c87249beb50964eef8f1180fc876
OpenAst <isrealme0@gmail.com>
2023-07-30 16:19:02 -0700

    tasks updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,45 +1,66 @@
 #!/usr/bin/python3
 """
-app
+Flask App that integrates with AirBnB static HTML Template
 """
-
-from flask import Flask, jsonify
-from flask_cors import CORS
-from os import getenv
-
 from api.v1.views import app_views
+from flask import Flask, jsonify, make_response, render_template, url_for
 from models import storage
+import os
+from werkzeug.exceptions import HTTPException
 
 
+# flask setup
 app = Flask(__name__)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
+# begin flask page rendering
 @app.teardown_appcontext
-def teardown(exception):
+def teardown_db(exception):
     """
-    teardown function
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
     """
     storage.close()
 
 
-@app.errorhandler(404)
-def handle_404(exception):
+@app.errorhandler(Exception)
+def global_error_handler(err):
     """
-    handles 404 error
-    :return: returns 404 json
+        Global Route to handle All Error Status Codes
     """
-    data = {
-        "error": "Not found"
-    }
+    if isinstance(err, HTTPException):
+        if type(err).__name__ == 'NotFound':
+            err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
 
-    resp = jsonify(data)
-    resp.status_code = 404
 
-    return(resp)
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
+
+
+def run_main_app():
+    """
+        Executes main functionality of the Flask Web App
+    """
+    setup_global_errors()
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port)
+
 
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    """
+    MAIN Flask App
+    """
+    run_main_app()
